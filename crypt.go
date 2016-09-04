062e57b26ae42dc1274e0543dfda99ec6fdf683c
James Adam Buckland <james.adam.buckland@gmail.com>
2016-09-04 16:22:10 -0500

    caesar encryption

--- crypt.go
+++ crypt.go
@@ -4,7 +4,8 @@ import "fmt"
 import "io/ioutil"
 import "strings"
 import "github.com/pborman/getopt"
-
+import "strconv"
+//import "reflect"
 
 func check(e error) {
   if e != nil {
@@ -12,7 +13,7 @@ func check(e error) {
   } 
 }
 
-type sett struct {
+type settings struct {
   encrypting bool
   decrypting bool
   existsInputPath bool
@@ -20,72 +21,118 @@ type sett struct {
   inputPath string
   outputPath string
   cipher string
+  args []string
 }
 
-func printStatus(settings sett, inputText string, outputText string) {
+func printStatus(world settings, inputText string, outputText string) {
   fmt.Println("")
-  fmt.Println("    cipher ::", settings.cipher)
+  fmt.Println("    cipher ::", world.cipher)
 
-  //print coding status
-  if settings.encrypting {
+  if world.encrypting {
     fmt.Println("    status :: encrypting")
-  } else if settings.decrypting {
+  } else if world.decrypting {
     fmt.Println("    status :: decrypting")
   } else {
     fmt.Println("    status :: neither encrypting nor decryptinng")
   }
   
-  //print in/output paths
-  fmt.Println(" inputPath ::", settings.inputPath)
-  if (settings.existsOutputPath) {
-    fmt.Println("outputPath ::", settings.outputPath)
+  fmt.Println(" inputPath ::", world.inputPath)
+  if (world.existsOutputPath) {
+    fmt.Println("outputPath ::", world.outputPath)
   }
 
   fmt.Println(" inputText :: <", strings.TrimSpace(inputText), ">")
 
-  if (settings.existsOutputPath) {
-    fmt.Println("outputText :: printed to", settings.outputPath)
+  if (world.existsOutputPath) {
+    fmt.Println("outputText :: printed to", world.outputPath)
   } else {
-    fmt.Println("outputText ::", outputText)
+    fmt.Println("outputText :: <", outputText, ">")
   }
 
   fmt.Println("")
 }
 
+func shift(r rune, shift int) rune {
+  //fmt.Println(string(r), r, int(r)+shift, string(int(r)+shift))
+  if( 65<=r && r<=90 ) {
+    tmp := int(r) - 65
+    tmp = tmp + shift
+    tmp = tmp % 26
+    return rune(tmp + 65)
+  }
+  if( 97<=r && r<=122 ) {
+    tmp := int(r) - 97
+    tmp = tmp + shift
+    tmp = tmp % 26
+    return rune(tmp + 97)
+  }
+  return r
+}
+
+func caesarEncrypt(inputText string, args []string) string {
+  n, err := strconv.Atoi(args[0])
+  check(err)
+  return strings.Map(func(r rune) rune {
+    return shift(r, n)
+  }, inputText)
+}
+
+func caesarDecrypt(inputText string, args []string) string {
+  n, err := strconv.Atoi(args[0])
+  check(err)
+  return string(n)
+}
+
+func process(inputText string, world settings) string {
+  if (world.cipher == "caesar"){
+    if (world.encrypting) {
+      return caesarEncrypt(inputText, world.args)
+    } else if (world.decrypting) {
+      return caesarDecrypt(inputText, world.args)
+    }
+  } else {
+    return "no such cipher"
+  }
+  return ""
+}
 
 func main() {
 
   encryptingFlag := getopt.BoolLong("encrypt", 'e', "encrypting?")
   decryptingFlag := getopt.BoolLong("decrypt", 'd', "decrypting?")
   quietFlag      := getopt.BoolLong("quiet", 'q', "quiet?")
-  inputPath      := getopt.StringLong("inputpath", 'i', "path to input file")
-  outputPath     := getopt.StringLong("outputpath",'o', "path to output file")
+  inputPath      := getopt.StringLong("inputpath", 'i', "", "path to input file")
+  outputPath     := getopt.StringLong("outputpath",'o', "", "path to output file")
   cipherPtr      := getopt.StringLong("cipher", 'c', "which cipher to use")
 
   getopt.Parse()
 
-  settings := sett{}
-  settings.encrypting = *encryptingFlag
-  settings.decrypting = *decryptingFlag
-  settings.cipher = *cipherPtr
-  settings.inputPath = *inputPath
-  settings.outputPath = *outputPath
-  settings.existsInputPath = (*inputPath != "")
-  settings.existsOutputPath = (*outputPath != "")
+  world                 := settings{}
+  world.encrypting       = *encryptingFlag
+  world.decrypting       = *decryptingFlag
+  world.cipher           = *cipherPtr
+  world.inputPath        = *inputPath
+  world.outputPath       = *outputPath
+  world.existsInputPath  = (*inputPath != "")
+  world.existsOutputPath = (*outputPath != "")
+  world.args             = getopt.Args()
 
   inputTextBytes, inputTextErr := ioutil.ReadFile(*inputPath)
   check(inputTextErr)
-  inputText := string(inputTextBytes)
+  inputText := strings.TrimSpace(string(inputTextBytes))
 
-  outputText := "whatever, man"
+  outputText := process(inputText, world)
 
-  if (settings.existsOutputPath) {
+  if (!*quietFlag){
+    printStatus(world, inputText, outputText)
+  }
+
+  if (world.existsOutputPath) {
     err := ioutil.WriteFile(*outputPath, []byte(outputText), 0644)
     check(err)
   }
 
-  if (!*quietFlag){
-    printStatus(settings, inputText, outputText)
-  }
+  //fmt.Println("args:", getopt.Args())
+  //fmt.Println(reflect.TypeOf(getopt.Args()))
 
 }
